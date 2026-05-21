948f34c49ec7911a0adf95542d6b225da2a22702
Qolbu Dzikru <61232165+qolbudr@users.noreply.github.com>
2026-05-21 01:43:24 +0000

    fix: add chmod symlink.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ proot-distro login ubuntu -- bash -c "cd /storage/emulated/0 && rm -rf *kbot*"
 proot-distro login ubuntu -- bash -c "mkdir -p /storage/emulated/0/kbot-main/1 && mv ~/kbot-tmp/accounts /storage/emulated/0/kbot-main/1 && mv ~/kbot-tmp/binary/kbot-linux-arm ~ && chmod +x ~/kbot-linux-arm"
 proot-distro login ubuntu -- bash -c "ln -s /storage/emulated/0/kbot-main/1/accounts ~/accounts"
 proot-distro login ubuntu -- bash -c "rm -rf ~/kbot-tmp"
-proot-distro login ubuntu -- bash -c "curl -fsSL https://raw.githubusercontent.com/qolbudr/public-kbot/refs/heads/main/symlink.sh -o symlink.sh"
+proot-distro login ubuntu -- bash -c "curl -fsSL https://raw.githubusercontent.com/qolbudr/public-kbot/refs/heads/main/symlink.sh -o symlink.sh && chmod +x symlink.sh"
 proot-distro login ubuntu -- bash -c "echo '~/symlink.sh' >> ~/.bashrc"
 echo 'proot-distro login ubuntu' >> ~/.bashrc
