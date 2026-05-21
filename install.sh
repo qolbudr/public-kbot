4f7194a652de3a19e7e2bf7268f57b6a28315736
Qolbu Dzikru <61232165+qolbudr@users.noreply.github.com>
2026-05-21 01:18:59 +0000

    fix: symlink bash

--- install.sh
+++ install.sh
@@ -9,36 +9,6 @@ proot-distro login ubuntu -- bash -c "cd /storage/emulated/0 && rm -rf *kbot*"
 proot-distro login ubuntu -- bash -c "mkdir -p /storage/emulated/0/kbot-main/1 && mv ~/kbot-tmp/accounts /storage/emulated/0/kbot-main/1 && mv ~/kbot-tmp/binary/kbot-linux-arm ~ && chmod +x ~/kbot-linux-arm"
 proot-distro login ubuntu -- bash -c "ln -s /storage/emulated/0/kbot-main/1/accounts ~/accounts"
 proot-distro login ubuntu -- bash -c "rm -rf ~/kbot-tmp"
-
-proot-distro login ubuntu -- bash -c "
-cat << 'EOF' > ~/setup-symlink.sh
-while true; do
-    clear
-    read -p 'Enter account folder [default: 1]: ' num
-    num=\${num:-1}
-    if [[ \"\$num\" =~ ^[0-9]+\$ ]]; then
-        ACCOUNT_PATH=\"/storage/emulated/0/kbot-main/\$num/accounts\"
-        if [ -d \"\$ACCOUNT_PATH\" ]; then
-            break
-        else
-            read -p \"Folder not found. Create it? (y/n): \" confirm
-            if [[ \"\$confirm\" == 'y' || \"\$confirm\" == 'Y' ]]; then
-                mkdir -p \"\$ACCOUNT_PATH\"
-                echo \"Created \$ACCOUNT_PATH\"
-                break
-            fi
-        fi
-    else
-        echo 'Invalid input. Please enter a number.'
-    fi
-done
-rm -f ~/accounts
-ln -s \"\$ACCOUNT_PATH\" ~/accounts
-~/kbot-linux-arm
-EOF
-chmod +x ~/setup-symlink.sh
-"
-
+proot-distro login ubuntu -- bash -c "curl -fsSL https://raw.githubusercontent.com/qolbudr/public-kbot/refs/heads/main/symlink.sh -o symlink.sh"
+proot-distro login ubuntu -- bash -c "echo '~/symlink.sh' > ~/.bashrc"
 echo 'proot-distro login ubuntu' > ~/.bashrc
-proot-distro login ubuntu -- bash -c "echo '~/setup-symlink.sh' > ~/.bashrc"
-proot-distro login ubuntu
