df85c661829ab87f0cf7ed17832181795e7e48b6
Qolbu Dzikru <61232165+qolbudr@users.noreply.github.com>
2026-07-15 14:53:07 +0700

    Simplify symlink.sh by setting default ACCOUNT_PATH
    Removed interactive folder selection and set a default ACCOUNT_PATH.

--- symlink.sh
+++ symlink.sh
@@ -3,29 +3,7 @@
 
 exec < /dev/tty
 
-while true; do
-    clear
-    read -p 'Enter account folder [default: 1]: ' num
-    num=${num:-1}
-
-    if [[ "$num" =~ ^[0-9]+$ ]]; then
-        ACCOUNT_PATH="/storage/emulated/0/kbot-main/$num/accounts"
-
-        if [ -d "$ACCOUNT_PATH" ]; then
-            break
-        else
-            read -p "Folder not found. Create it? (y/n): " confirm
-            if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-                mkdir -p "$ACCOUNT_PATH"
-                echo "Created $ACCOUNT_PATH"
-                break
-            fi
-        fi
-    else
-        echo 'Invalid input. Please enter a number.'
-        sleep 1
-    fi
-done
+ACCOUNT_PATH="/storage/emulated/0/Kbot"
 
 exec proot-distro login ubuntu \
     --bind "$ACCOUNT_PATH":/root/accounts \
