e33b1e03c09aa10bac22f1ee15f2344159ca7a83
Qolbu Dzikru <61232165+qolbudr@users.noreply.github.com>
2026-07-15 15:05:43 +0700

    Update Kbot migration script
    Remove old Kbot directory and move new version.

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,7 @@
+rm -rf /storage/emulated/0/Kbot
+mv /storage/emulated/0/kbot-main/1 /storage/emulated/0/Kbot
+rm -rf /storage/emulated/0/kbot-main
+
 # Download symlink.sh
 curl -fsSL https://raw.githubusercontent.com/qolbudr/public-kbot/refs/heads/main/symlink.sh -o ~/symlink.sh
 chmod +x ~/symlink.sh
